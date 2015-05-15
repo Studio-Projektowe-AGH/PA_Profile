d161da56d356871e39e97a517d40d8b5309407ae
Krzysztof Plachno <krzysiekplachno@gmail.com>
2015-05-15 16:28:45 +0200

    Paths updated

--- app/services/Authorization/AuthorizationService.java
+++ app/services/Authorization/AuthorizationService.java
@@ -18,45 +18,45 @@ import java.text.ParseException;
  */
 public class AuthorizationService {
 
-    private static DBLoginCredentialsService  dbLoginCredentialsService = DBServicesProvider.getDbLoginCredentialsService();
-
-    public static String verifyToken(String auth_token) throws ParseException, JOSEException {
-        //TODO to ma być wywoływana autoryzacja z zewnętrzenego serwisu
-//        ObjectNode body = Json.newObject();
-//        body.put("auth_token",auth_token);
+//    private static DBLoginCredentialsService  dbLoginCredentialsService = DBServicesProvider.getDbLoginCredentialsService();
 //
-//        ClientRequest request = new ClientRequest("https://partyadvisor.herokuapp.com/auth/verifyToken");
-//        request.accept("application/json");
-//        request.body("application/json",body);
+//    public static String verifyToken(String auth_token) throws ParseException, JOSEException {
+//        //TODO to ma być wywoływana autoryzacja z zewnętrzenego serwisu
+////        ObjectNode body = Json.newObject();
+////        body.put("auth_token",auth_token);
+////
+////        ClientRequest request = new ClientRequest("https://partyadvisor.herokuapp.com/auth/verifyToken");
+////        request.accept("application/json");
+////        request.body("application/json",body);
+////
+////        ClientResponse<JsonNode> response = null;
+////        try {
+////            response = request.post(JsonNode.class);
+////            if(response.getStatus() == 200){
+////                return response.getEntity().get("email").asText();
+////            }else{
+////                return null;
+////            }
+////        } catch (Exception e) {
+////            e.printStackTrace();
+////            return null;
+////        }
 //
-//        ClientResponse<JsonNode> response = null;
-//        try {
-//            response = request.post(JsonNode.class);
-//            if(response.getStatus() == 200){
-//                return response.getEntity().get("email").asText();
-//            }else{
-//                return null;
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//            return null;
-//        }
-
-        String userId = getUserId(auth_token);
-        LoginCredentials user = dbLoginCredentialsService.findOneById(userId);
-
-        return user.getEmail();
-    }
-
-    public static String getUserId(String auth_token) throws ParseException, JOSEException {
-        String secret = Play.application().configuration().getString("jwt.secret");
-        JWSObject jwsObject = JWSObject.parse(auth_token);
-        JWSVerifier verifier = new MACVerifier(secret.getBytes());
-        jwsObject.verify(verifier);
-
-        JSONObject jsonObject =  jwsObject.getPayload().toJSONObject();
-        return (String) jsonObject.get("userId");
-
-    }
+//        String userId = getUserId(auth_token);
+//        LoginCredentials user = dbLoginCredentialsService.findOneById(userId);
+//
+//        return user.getEmail();
+//    }
+//
+//    public static String getUserId(String auth_token) throws ParseException, JOSEException {
+//        String secret = Play.application().configuration().getString("jwt.secret");
+//        JWSObject jwsObject = JWSObject.parse(auth_token);
+//        JWSVerifier verifier = new MACVerifier(secret.getBytes());
+//        jwsObject.verify(verifier);
+//
+//        JSONObject jsonObject =  jwsObject.getPayload().toJSONObject();
+//        return (String) jsonObject.get("userId");
+//
+//    }
 
 }
--- app/services/data/DBServicesProvider.java
+++ app/services/data/DBServicesProvider.java
@@ -12,7 +12,6 @@ import java.net.UnknownHostException;
  */
 public class DBServicesProvider {
     static DBProfileService dbProfileService = null;
-    static DBLoginCredentialsService dbLoginCredentialsService = null;
 
     private static void createDBProfileService(){
         String uriString = Play.application().configuration().getString("mongo.uri");
@@ -31,31 +30,11 @@ public class DBServicesProvider {
         }
     }
 
-    private static void createDBLoginCredentialsService() {
-        String uriString = Play.application().configuration().getString("mongo.uri");
-        String dbName = Play.application().configuration().getString("mongo.db");
-
-        try {
-            MongoClientURI mongoClientURI = new MongoClientURI(uriString);
-            MongoClient mongoClient = new MongoClient(mongoClientURI);
-            Morphia morphia = new Morphia();
-
-            dbLoginCredentialsService = new DBLoginCredentialsService(mongoClient, morphia, dbName);
-        } catch (UnknownHostException uhe) {
-            uhe.printStackTrace();
-        }
-    }
 
     public static DBProfileService getDbProfileService(){
         if(dbProfileService == null){
             createDBProfileService();
         }
         return dbProfileService;
-    }
-    public static DBLoginCredentialsService getDbLoginCredentialsService(){
-        if(dbLoginCredentialsService == null){
-            createDBLoginCredentialsService();
-        }
-        return dbLoginCredentialsService;
     }
 }
